56c1d83a7a405f92d3920dd7972ccb8559da1969
AnikHaque <ekramul15-811@diu.edu.bd>
2024-07-03 13:50:18 +0600

    analytics service part done

--- src/Components/IT-Service/Analysis-Business/AnalysisBusiness.jsx
+++ src/Components/IT-Service/Analysis-Business/AnalysisBusiness.jsx
@@ -1,15 +1,19 @@
 import AnalysisBusinessBanner from "./AnalysisBusinessBanner";
 import AnalysisBusinessFuture from "./AnalysisBusinessFuture";
+import AnalyticsServiceProvide from "./AnalyticsServiceProvide";
 import BusinessAnalysisIntro from "./BusinessAnalysisIntro";
 import BusinessWork from "./BusinessWork";
+import DataAnalyticsIntro from "./DataAnalyticsIntro";
 
 const AnalysisBusiness = () => {
   return (
     <div>
       <AnalysisBusinessBanner></AnalysisBusinessBanner>
       <BusinessAnalysisIntro></BusinessAnalysisIntro>
+      <AnalyticsServiceProvide></AnalyticsServiceProvide>
       <BusinessWork></BusinessWork>
       <AnalysisBusinessFuture></AnalysisBusinessFuture>
+      <DataAnalyticsIntro></DataAnalyticsIntro>
     </div>
   );
 };
