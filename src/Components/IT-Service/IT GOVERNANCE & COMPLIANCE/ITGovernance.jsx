51ecf0e47a114ce8ff3e0db434efc281cfdbc3f5
AnikHaque <ekramul15-811@diu.edu.bd>
2024-07-03 20:31:28 +0600

    added IT Governance

--- src/Components/IT-Service/IT GOVERNANCE & COMPLIANCE/ITGovernance.jsx
+++ src/Components/IT-Service/IT GOVERNANCE & COMPLIANCE/ITGovernance.jsx
@@ -1,9 +1,15 @@
+import AchieveMaintainCompliance from "./AchieveMaintainCompliance";
 import ITGovernanceBanner from "./ITGovernanceBanner";
+import ITGovernanceFoundation from "./ITGovernanceFoundation";
+import ITGovernanceIntro from "./ITGovernanceIntro";
 
 const ITGovernance = () => {
   return (
     <div>
       <ITGovernanceBanner></ITGovernanceBanner>
+      <ITGovernanceIntro></ITGovernanceIntro>
+      <ITGovernanceFoundation></ITGovernanceFoundation>
+      <AchieveMaintainCompliance></AchieveMaintainCompliance>
     </div>
   );
 };
