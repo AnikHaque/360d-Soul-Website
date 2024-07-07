4725d08e7a6d22f4e4b458bd7ae3b2089f8a0b5e
AnikHaque <ekramul15-811@diu.edu.bd>
2024-07-07 16:47:20 +0600

    added talent management

--- src/Components/Solutions/HR-Strategy/HRStrategy.jsx
+++ src/Components/Solutions/HR-Strategy/HRStrategy.jsx
@@ -1,11 +1,13 @@
 import AboutHRStrategy from "./AboutHRStrategy";
 import HRStrategyBanner from "./HRStrategyBanner";
+import TalentManagement from "./TalentManagement/TalentManagement";
 
 const HRStrategy = () => {
   return (
     <div>
       <HRStrategyBanner></HRStrategyBanner>
       <AboutHRStrategy></AboutHRStrategy>
+      <TalentManagement></TalentManagement>
     </div>
   );
 };
