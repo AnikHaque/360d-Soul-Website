271429354cd88c9f2f6e1238ee479791670b7c57
AnikHaque <ekramul15-811@diu.edu.bd>
2024-07-07 17:42:55 +0600

    updated compensation survey

--- src/Components/Solutions/HR-Strategy/Compensation-Survey/CompensationSurvey.jsx
+++ src/Components/Solutions/HR-Strategy/Compensation-Survey/CompensationSurvey.jsx
@@ -1,13 +1,13 @@
 const CompensationSurvey = () => {
   return (
     <div>
-      <section className="py-10 bg-[#333e68] sm:py-16 lg:py-24">
+      <section className="py-10 bg-gray-50 sm:py-16 lg:py-24">
         <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
           <div className="max-w-xl mx-auto text-center">
-            <h2 className="text-3xl font-bold leading-tight text-white sm:text-4xl lg:text-5xl">
+            <h2 className="text-3xl font-normal leading-tight text-black sm:text-4xl lg:text-5xl">
               Compensation Survey helps you to Survey
             </h2>
-            <p className="mt-4 text-base leading-relaxed text-gray-100 text-justify">
+            <p className="mt-4 text-base leading-relaxed text-gray-700 text-justify">
               A compensation survey is a tool used by organizations to gather
               and analyze data on salaries, benefits, and other compensation
               components across similar industries or job roles. This
@@ -51,7 +51,7 @@ const CompensationSurvey = () => {
           <div className="flex flex-col items-start justify-center max-w-lg px-10 mx-auto mt-8 space-y-8 lg:max-w-xl sm:px-0 sm:space-y-0 sm:flex-row sm:mt-16 sm:space-x-6 lg:space-x-12 sm:items-center">
             <div className="flex items-center flex-1">
               <svg
-                className="flex-shrink-0 text-white/50 w-14 h-14"
+                className="flex-shrink-0 text-black/50 w-14 h-14"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
@@ -64,14 +64,14 @@ const CompensationSurvey = () => {
                   d="M3 4a1 1 0 011-1h16a1 1 0 011 1v2.586a1 1 0 01-.293.707l-6.414 6.414a1 1 0 00-.293.707V17l-4 4v-6.586a1 1 0 00-.293-.707L3.293 7.293A1 1 0 013 6.586V4z"
                 />
               </svg>
-              <p className="ml-4 text-lg font-semibold leading-snug text-white">
+              <p className="ml-4 text-lg font-semibold leading-snug text-black">
                 Create powerful websites fast
               </p>
             </div>
 
             <div className="flex items-center flex-1">
               <svg
-                className="flex-shrink-0 text-white/50 w-14 h-14"
+                className="flex-shrink-0 text-black/50 w-14 h-14"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
@@ -84,7 +84,7 @@ const CompensationSurvey = () => {
                   d="M13 10V3L4 14h7v7l9-11h-7z"
                 />
               </svg>
-              <p className="ml-4 text-lg font-semibold leading-snug text-white">
+              <p className="ml-4 text-lg font-semibold leading-snug text-black">
                 Easy to customize, ready to launch
               </p>
             </div>
